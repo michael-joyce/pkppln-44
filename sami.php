56b698fd420eb9325a6eb51758fb3d578a494c53
Michael Joyce <ubermichael@gmail.com>
2019-10-31 13:05:37 -0700

    config, submodules, and github templates.

--- sami.php
+++ sami.php
@@ -6,26 +6,18 @@ use Symfony\Component\Finder\Finder;
 
 $dir = __DIR__;
 
-$iterator = Finder::create()
-        ->files()
-        ->name('*.php')
-        ->exclude('Resources')
-        ->exclude('Tests')
-        ->in($dir . '/src/AppBundle');
-
-$options = array(
-    // 'theme' => 'symfony',
-    'title' => 'PKP PN Internals',
-    'build_dir' => $dir . '/web/docs/api/sami',
-    'cache_dir' => $dir . '/var/cache/sami',
-    'remote_repository' => new GitHubRemoteRepository('ubermichael/pkppln', $dir),
+$config = array(
+//    'theme'                => 'symfony',
+    'title'                => 'Circus Internal API',
+    'build_dir'            => $dir . '/web/docs/api',
+    'cache_dir'            => $dir . '/var/cache/sami',
+    'remote_repository'    => new GitHubRemoteRepository('ubermichael/circus', $dir),
     'default_opened_level' => 2,
-    'include_parent_data' => true,
-    'insert_todos' => true,
-    'sort_class_properties' => true,
-    'sort_class_methods' => true,
-    'sort_class_traits' => true,
-    'sort_class_interfaces' => true,
 );
 
-return new Sami($iterator, $options);
+$iterator = Finder::create()
+    ->files()
+    ->name('*.php')
+    ->in('src');
+
+return new Sami($iterator, $config);
